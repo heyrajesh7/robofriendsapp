64c13b15cba1a7d3e8bd73180749a70bcb48b48c
heyrajesh7 <67043356+heyrajesh7@users.noreply.github.com>
2020-08-07 12:21:13 +0530

    Add files via upload

---
+++ precache-manifest.a70b54b209a685ef0cce5a4456328621.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2efc7102f3345261673839ce6b9e2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0084f20f85b4cf7d45b9",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b85854f8e0b9411c75e9",
+    "url": "/static/css/main.44d3a215.chunk.css"
+  },
+  {
+    "revision": "0084f20f85b4cf7d45b9",
+    "url": "/static/js/2.a3e681fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3e681fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b85854f8e0b9411c75e9",
+    "url": "/static/js/main.64e7829e.chunk.js"
+  },
+  {
+    "revision": "be53ff6425745a6913e6",
+    "url": "/static/js/runtime-main.2548a010.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  }
+]);
---
+++ robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Cristiano Ronaldo',
+      username: 'CR7',
+      teamname: 'Juventus FC'
+    },
+    {
+      id: 2,
+      name: 'Lionel Messi',
+      username: 'Leo',
+      teamname: 'Barcelona FC'
+    },
+    {
+      id: 3,
+      name: 'Neymar',
+      username: 'Ney10',
+      teamname: 'Paris Saint Germain FC'
+    },
+    {
+      id: 4,
+      name: 'Mesut Ozil',
+      username: 'Ozil',
+      teamname: 'Arsenal FC'
+    },
+    {
+      id: 5,
+      name: 'Sergio Ramos',
+      username: 'Ramos',
+      teamname: 'Real Madrid FC'
+    },
+    {
+      id: 6,
+      name: 'Harry Kane',
+      username: 'Kane',
+      teamname: 'Tottenhem Hotspurs FC'
+    },
+    {
+      id: 7,
+      name: 'Mo Sallah',
+      username: 'Mo',
+      teamname: 'Liverpool FC'
+    },
+    {
+      id: 8,
+      name: 'Chistian Pulisic',
+      username: 'Pulisic',
+      teamname: 'Chelsea FC'
+    },
+    {
+      id: 9,
+      name: 'Raheem Sterling',
+      username: 'Sterling',
+      teamname: 'Manchester City FC'
+    },
+    {
+      id: 10,
+      name: 'Bruno Fernandes',
+      username: 'Bruno',
+      teamname: 'Manchester United FC'
+    }
+  ];
+  
